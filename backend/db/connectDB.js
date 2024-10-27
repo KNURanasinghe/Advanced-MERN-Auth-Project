27aeea6d2d8b1f27bb48d852d760a801c0c60814
KNURanasinghe <113103000+KNURanasinghe@users.noreply.github.com>
2024-10-27 18:34:22 +0530

    error with

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log("uri",conn);
+        //console.log("uri",conn);
         console.log(`MongoDB Connected: ${conn.connection.host}`)
     } catch (error) {
         console.log("Error connection to MongoDB: ", error.message);
